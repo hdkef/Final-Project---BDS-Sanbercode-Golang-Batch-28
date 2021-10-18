5cb275ed3129b4903453789e38df6a36e9f475c3
fedora <hdkef11@gmail.com>
2021-10-18 08:27:32 +0800

    ch authorization header name

--- constantName/constantName.go
+++ constantName/constantName.go
@@ -1,7 +1,7 @@
 package constantname
 
 const USER_CTX = "user"
-const AUTHORIZATION_HEADER = "authorization"
+const AUTHORIZATION_HEADER = "Authorization"
 const DB = "db"
 const TOKEN_EXP_IN_HOUR = 12
 const TOKEN_TYPE_NEW = "new"
